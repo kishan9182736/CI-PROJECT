2d613a1099d90d3427b978ff990a41e4a1a31605
kishan <108215645+kishan9182736@users.noreply.github.com>
2025-07-30 01:54:48 +0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,6 @@ set -e
 # pull the docker image from Docker Hub
  docker pull kishan273/simple-python-flask-service
 
+
 #Run the Docker image as a container 
 docker run -d -p 5000:5000 kishan273/simple-python-flask-service
