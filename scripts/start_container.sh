11382218d567d206fa65dd8f8903f2f6bd680a50
kishan <108215645+kishan9182736@users.noreply.github.com>
2025-08-01 11:59:29 +0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker pull kishan273/simple-python-flask-service
-docker run -d -p 5000:5000 kishan273/simple-python-flask-service
+sudo docker stop python-app || true
+sudo docker rm python-app || true
+sudo docker run -d -p 5000:5000 --name python-app \
+  kishan273/simple-python-flask-service \
+  sh -c "python app.py --host=0.0.0.0 --port=5000"
